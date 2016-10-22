a90c0192c231e3ec6b610e1898627bd2e244151c
Anthony Howell <anthonyrhowell@gmail.com>
2016-10-22 14:51:48 -0500

    more cowbell

---
+++ src/main/java/io/ctl/globalhack/repository/ShelterRepository.java
@@ -0,0 +1,7 @@
+package io.ctl.globalhack.repository;
+
+import io.ctl.globalhack.model.Shelter;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ShelterRepository extends CrudRepository<Shelter, String> {
+}
