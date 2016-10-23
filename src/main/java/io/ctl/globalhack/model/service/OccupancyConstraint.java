10b0ffaace95aedeb128ec3360be319d64c64fc0
Keith Homco <keith.homco@centurylink.com>
2016-10-22 21:07:09 -0500

    fixed broken stuff

--- src/main/java/io/ctl/globalhack/model/service/OccupancyConstraint.java
+++ src/main/java/io/ctl/globalhack/model/service/OccupancyConstraint.java
@@ -4,5 +4,7 @@ package io.ctl.globalhack.model.service;
  * Created by khomco on 10/22/16.
  */
 public enum OccupancyConstraint {
-    FAMILY, WOMEN, MEN, YOUTH;
+    ACCEPTS_FAMILY, ACCEPTS_WOMEN, ACCEPTS_MEN,
+    ACCEPTS_YOUTH, ACCEPT_SEX_OFFENDER, REQUIRES_REFERRAL,
+    REQUIRES_HIV, ACCEPTS_HIV, ACCEPTS_PREGRANT, REQUIRES_SSN;
 }
